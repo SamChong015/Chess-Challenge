fc55c42d9e723b9a762813f616e3d53d1b02b834
Samantha Chong <Samantha.chong@soth.ca>
2023-08-21 10:57:20 -0400

    Started Planning Bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,9 +1,27 @@
 ﻿using ChessChallenge.API;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text.RegularExpressions;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        if(IsInCheck())
+        {
+            //move so not in check
+        }
+        if(first move)
+        {
+            //move pawn in front of king one square forward
+        }
+        else if(second move)
+        {
+            //capture highest value peice, where queen is safe
+        }
+        else
+        {
+            //move queen to random square where safe
+        }
         Move[] moves = board.GetLegalMoves();
         return moves[0];
     }
